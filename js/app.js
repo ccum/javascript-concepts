94e7eb0dae4f5c5969ab8c74ec0608541ca2d589
Cesar Cueva <cesar.cueva@bbva.com>
2019-06-12 18:52:24 -0500

    variables

---
+++ js/app.js
@@ -0,0 +1,13 @@
+// crear variables con var
+var aprendiendo = 'JavaScript'
+console.log(aprendiendo)
+
+//crear variables con const
+
+const aprendiendo1='Cambios'
+
+//crear variables con let
+
+let aprendiendo2 = "cambios2"
+aprendiendo2="tres"
+console.log(aprendiendo2)

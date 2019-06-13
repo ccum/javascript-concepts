22f860c345cde02ee8d1b38b97d4290120309b07
Cesar Cueva <cesar.cueva@bbva.com>
2019-06-12 19:11:41 -0500

    Funciones y los distintos tipos de funciones

--- js/app.js
+++ js/app.js
@@ -1,13 +1,7 @@
-const nombre = "Cesar"
-const trabajo = "Desarrollador"
-console.log(`Nombre: ${nombre}, Trabajo: ${trabajo}`)
+// function declaration
+// function expression
+const cliente = function(nombreCliente){
+    console.log("mostrando datos del cliente " + nombreCliente)
+}
 
-
-const contenedorApp = document.querySelector("#app");
-let html = `
-    <ul>
-        <li>${nombre}</li>
-        <li>${trabajo}</li>
-    <ul>
-`;
-contenedorApp.innerHTML=html;
+cliente("Cesar")

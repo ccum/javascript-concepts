5c61f88ad03dedadbcedaa7a550d8491f8cc1232
Cesar Cueva <cesar.cueva@bbva.com>
2019-06-12 19:18:00 -0500

    Funciones con parametros

--- js/app.js
+++ js/app.js
@@ -1,7 +1,17 @@
-// function declaration
-// function expression
-const cliente = function(nombreCliente){
-    console.log("mostrando datos del cliente " + nombreCliente)
+//parametros por defecto
+
+function actividad(nombre='WalterWhite', actividad='Enseñar Quiminca'){
+    console.log(`la persona ${nombre} realiza ${actividad}`)
 }
 
-cliente("Cesar")
+actividad('Cesar','developer');
+actividad('Pedro','developer');
+
+
+const another_actividad = function(nombre='WalterWhite', actividad='Enseñar Quiminca'){
+    console.log(`la persona ${nombre} realiza ${actividad}`)
+}
+
+
+another_actividad('Cesar','developer');
+another_actividad('Pedro','developer');

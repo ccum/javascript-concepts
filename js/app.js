ac4ac3ea1dd3964e4e325811e146edc6d4b2f622
Cesar Cueva <cesar.cueva@bbva.com>
2019-06-18 14:28:59 -0500

    Agregar prototype

--- js/app.js
+++ js/app.js
@@ -4,10 +4,16 @@ function Tarea(nombre, urgencia){
     this.nombre = nombre;
     this.urgencia = urgencia;
 }
+// Agregar prototype
+Tarea.prototype.mostrarInformacionTarea= function(){
+    return `La tarea ${this.nombre} tiene una prioridad de ${this.urgencia}`
+}
 
 //crear tarea
 
 const tarea1 = new Tarea('Aprender React', 'Urgente');
 console.log(tarea1);
+console.log(tarea1.mostrarInformacionTarea());
 const tarea2 = new Tarea('Preparar cafe', 'Urgente');
 console.log(tarea2);
+console.log(tarea2.mostrarInformacionTarea());

df9e30f8a672173cd9703d05c05fc4e422b53db5
Cesar Cueva <cesar.cueva@bbva.com>
2019-06-18 14:20:17 -0500

    object constructor

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
-//objects
-// objects literal
+// object constructor
 
-const persona = {
-    nombre:'Juan',
-    pefesion: 'Developer',
-    edad: 32
+function Tarea(nombre, urgencia){
+    this.nombre = nombre;
+    this.urgencia = urgencia;
 }
 
-console.log(persona);
-console.log(persona.nombre);
-console.log(persona['edad']);
+//crear tarea
+
+const tarea1 = new Tarea('Aprender React', 'Urgente');
+console.log(tarea1);
+const tarea2 = new Tarea('Preparar cafe', 'Urgente');
+console.log(tarea2);

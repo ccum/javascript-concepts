5254223adc11ac4716edc886926a1b7a7d07ca13
Cesar Cueva <cesar.cueva@bbva.com>
2019-07-21 17:17:24 -0500

    Modulos en ES6 y exportando funciones en modulos

--- js/app.js
+++ js/app.js
@@ -1,33 +1,6 @@
-// escribiendo clases
+import {nombreTarea,crearTarea,tareaCompletada} from './tareas.js' 
+console.log(nombreTarea);
 
-class Tarea{
-    constructor(nombre, prioridad){
-        this.nombre=nombre;
-        this.prioridad=prioridad;
-    }
-    mostrar(){
-        return `${this.nombre} tiene una prioridad de  ${this.prioridad}`;
-    }
-
-}
-
-class ComprasPendientes extends Tarea{
-    constructor(nombre,prioridad,cantidad){
-        super(nombre,prioridad)
-        this.cantidad=cantidad;
-    }
-    mostrar(){
-        return super.mostrar()
-    }
-}
-
-let tarea1 = new Tarea('Aprender Javascript', 'Alta');
-let tarea2 = new Tarea('Tomar cafe', 'Media');
-let tarea3 = new Tarea('Pasear al perro', 'Baja');
-
-console.log(tarea1.mostrar());
-console.log(tarea2.mostrar());
-console.log(tarea3.mostrar());
-
-let compra1 = new ComprasPendientes('Jabon', 'Urgente', 3);
-console.log(compra1.mostrar())
+const tarea1 = crearTarea('Pasear al perro', 'media');
+console.log(tarea1)
+tareaCompletada();
---
+++ js/tareas.js
@@ -0,0 +1,44 @@
+export const nombreTarea = 'Pasea al perro'
+
+
+//expoertar una funcion
+
+export const crearTarea= (tarea, urgencia) =>{
+    return `La tarea ${tarea} tiene una urgencua de ${urgencia}`;
+}
+
+export const tareaCompletada=() =>{
+    console.log('La tarea se completó')
+}
+// escribiendo clases
+class Tarea{
+    constructor(nombre, prioridad){
+        this.nombre=nombre;
+        this.prioridad=prioridad;
+    }
+    mostrar(){
+        return `${this.nombre} tiene una prioridad de  ${this.prioridad}`;
+    }
+
+}
+
+class ComprasPendientes extends Tarea{
+    constructor(nombre,prioridad,cantidad){
+        super(nombre,prioridad)
+        this.cantidad=cantidad;
+    }
+    mostrar(){
+        return super.mostrar()
+    }
+}
+
+let tarea1 = new Tarea('Aprender Javascript', 'Alta');
+let tarea2 = new Tarea('Tomar cafe', 'Media');
+let tarea3 = new Tarea('Pasear al perro', 'Baja');
+
+// console.log(tarea1.mostrar());
+// console.log(tarea2.mostrar());
+// console.log(tarea3.mostrar());
+
+let compra1 = new ComprasPendientes('Jabon', 'Urgente', 3);
+// console.log(compra1.mostrar())

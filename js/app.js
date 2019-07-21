9c438bc0743f225ab73bfa75dc507ce983e4f014
Cesar Cueva <cesar.cueva@bbva.com>
2019-07-21 11:52:00 -0500

    Promises con Ajax

--- js/app.js
+++ js/app.js
@@ -1,18 +1,42 @@
-// Promises
+// Promises con Ajax
 
-const aplicarDescuento = new Promise((resolve, reject)=>{
-    setTimeout(()=>{
-        let descuento = false;
-        if(descuento){
-            resolve('Descuento aplicado');
+const dascargarUsuarios = cantidad => new Promise((resolve, reject)=>{
+    //pasar la cantidad al api
+    const api = `https://randomuser.me/api/?results=${cantidad}&nat=us`;
+
+    //lammado ajax
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', api, true);
+    xhr.onload=() =>{
+        if(xhr.status === 200){
+            resolve(JSON.parse(xhr.responseText).results)
         }else{
-            reject('No se puede aplicar el descuento');
+            reject(Error(xhr.statusText));
         }
-    },3000);
+    }
+    xhr.onerror = (error) => reject(error);
+    xhr.send()
 });
 
-aplicarDescuento.then(resultado=>{
-    console.log(resultado)
-}).catch(error=>{
-    console.log(error)
-});
+dascargarUsuarios(30)
+.then(
+    miembros => imprimirHTML(miembros),
+    error => console.error(
+        new Error('Hubo un error' + error)
+        )
+    )
+
+function imprimirHTML(usuarios){
+    let html = '';
+    usuarios.forEach(element => {
+        html+=`
+            <li>
+                Nombre: ${element.name.first} ${element.name.last}
+                Pais: ${element.nat}
+                Imagen: <img src=${element.picture.medium}>
+            </li>
+        `
+    });
+    const contendorApp = document.querySelector('#app');
+    contendorApp.innerHTML = html;
+}

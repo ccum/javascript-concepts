8659310d6500dfb331139c256482198a0922ac5d
Cesar Cueva <cesar.cueva@bbva.com>
2019-07-21 10:19:05 -0500

    Promises

--- js/app.js
+++ js/app.js
@@ -1,33 +1,18 @@
-// Metodos en arraglos
+// Promises
 
-const personas =[
-    {nombre:'Cesar', edad:30, aprendiendo:'Javasript'},
-    {nombre:'Pablo', edad:20, aprendiendo:'PHP'},
-    {nombre:'Alejandra', edad:13, aprendiendo:'Adobe'},
-    {nombre:'Gael', edad:3, aprendiendo:'Python'},
-    {nombre:'Karen', edad:34, aprendiendo:'ReactJS'},
-    {nombre:'Miguel', edad:23, aprendiendo:'Java'},
-]
-
-console.log(personas);
-//mayores de 28
-const mayores = personas.filter(persona=>{
-    return persona.edad > 28;
+const aplicarDescuento = new Promise((resolve, reject)=>{
+    setTimeout(()=>{
+        let descuento = false;
+        if(descuento){
+            resolve('Descuento aplicado');
+        }else{
+            reject('No se puede aplicar el descuento');
+        }
+    },3000);
 });
-console.log(mayores);
 
-//find
-
-const alejandra= personas.find(persona=>{
-    return persona.nombre=== "Alejandra"
-})
-
-console.log('Alejandra esta aprendiendo ' + alejandra.aprendiendo);
-
-//promedio de edades
-
-let total = personas.reduce((edadTotal,persona)=>{
-    return edadTotal + persona.edad;
-},0);
-
-console.log(total / personas.length);
+aplicarDescuento.then(resultado=>{
+    console.log(resultado)
+}).catch(error=>{
+    console.log(error)
+});

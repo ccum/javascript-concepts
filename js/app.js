d68483aec540b54553c5b5a37e793cf484e72be6
Cesar Cueva <cesar.cueva@bbva.com>
2019-07-21 17:30:28 -0500

    Exportando Clases que heredan de otras

--- js/app.js
+++ js/app.js
@@ -1,6 +1,12 @@
-import {nombreTarea,crearTarea,tareaCompletada} from './tareas.js' 
-console.log(nombreTarea);
+import Tarea from './tareas.js' 
+import ComprasPendientes from './compras.js'
 
-const tarea1 = crearTarea('Pasear al perro', 'media');
+const tarea1 = new Tarea('Aprender JavaScript', 'Urgente')
+
 console.log(tarea1)
-tareaCompletada();
+console.log(tarea1.mostrar())
+
+const compra1 = new ComprasPendientes('Pizza', 'Urgente',2)
+console.log(compra1);
+console.log(compra1.mostrar());
+console.log(compra1.hola()) 
--- js/tareas.js
+++ js/tareas.js
@@ -1,17 +1,6 @@
-export const nombreTarea = 'Pasea al perro'
 
-
-//expoertar una funcion
-
-export const crearTarea= (tarea, urgencia) =>{
-    return `La tarea ${tarea} tiene una urgencua de ${urgencia}`;
-}
-
-export const tareaCompletada=() =>{
-    console.log('La tarea se completó')
-}
 // escribiendo clases
-class Tarea{
+export default class Tarea{
     constructor(nombre, prioridad){
         this.nombre=nombre;
         this.prioridad=prioridad;
@@ -22,23 +11,14 @@ class Tarea{
 
 }
 
-class ComprasPendientes extends Tarea{
-    constructor(nombre,prioridad,cantidad){
-        super(nombre,prioridad)
-        this.cantidad=cantidad;
-    }
-    mostrar(){
-        return super.mostrar()
-    }
-}
 
-let tarea1 = new Tarea('Aprender Javascript', 'Alta');
-let tarea2 = new Tarea('Tomar cafe', 'Media');
-let tarea3 = new Tarea('Pasear al perro', 'Baja');
+// let tarea1 = new Tarea('Aprender Javascript', 'Alta');
+// let tarea2 = new Tarea('Tomar cafe', 'Media');
+// let tarea3 = new Tarea('Pasear al perro', 'Baja');
 
 // console.log(tarea1.mostrar());
 // console.log(tarea2.mostrar());
 // console.log(tarea3.mostrar());
 
-let compra1 = new ComprasPendientes('Jabon', 'Urgente', 3);
+//let compra1 = new ComprasPendientes('Jabon', 'Urgente', 3);
 // console.log(compra1.mostrar())
